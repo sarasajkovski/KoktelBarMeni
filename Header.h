36a75b7aeee04496481df32da2b1d40ec9d08e62
sarasajkovski <sara.sajkovski@gmail.com>
2024-09-03 10:54:02 +0200

    Header.h

--- Header.h
+++ Header.h
@@ -1,17 +1,21 @@
 #ifndef HEADER_H
-#define HEADER_H
-
 #define MAX_LENGTH 50
+#define MAX_NARUDZBI 50
 
 typedef struct {         
     char prezime[MAX_LENGTH];
     int brojOsoba;
 } Rezervacija;
 
+Rezervacija* narudzbe[MAX_NARUDZBI];
+int brojNarudzbi;
 
+
+void unesiRezervaciju(Rezervacija** rez);
+void prikaziIzbornik();
 void prikaziGlavniIzbornik();
-void prviMenu(int izbor, char** fileIme);
-void ocistiUlaznitok();
+void ocistiUlazniTok();
+void prviMenu(int *izbor, char** fileIme);
 void ispisiCijeliKoktel(const char* nazivDatoteke);                                 
 void ispisiDetaljeKoktela(int odabirKoktela, int kategorija, FILE* racun);
 float racunanjeCijenaGin(const char* nazivDatoteke, float koktel1, float koktel2, float koktel3, FILE* racun);
@@ -20,12 +24,24 @@ float racunanjeCijenaTequilla(const char* nazivDatoteke, float koktel1, float ko
 float racunanjeCijenaRum(const char* nazivDatoteke, float koktel1, float koktel2, float koktel3, float koktel4, FILE* racun);
 float racunanjePica(const char* nazivDatoteke, float koktel1, float koktel2, float koktel3, float koktel4, float koktel5, float koktel6, FILE* racun);
 int dodatniOdabir( float* ukupnaCijena, FILE* racun);
-void unesiRezervaciju(Rezervacija** rez);
 void ispisUkupneCijene(float ukupnaCijena, FILE* racun);
+void ispisRezervacije(novaNarudzba, racun);
+
+void pregledNarudzbi();
+void izbrisiNarudzbu(int broj);
+void spremiNarudzbe();
+
+void promijeniBrojOsoba(Rezervacija* rezervacija, int noviBrojOsoba);
+void azurirajNarudzbe(Rezervacija* narudzbe[], int brojNarudzbi, const char* imeDatoteke);
+void azurirajRacun(Rezervacija* rezervacija, const char* imeDatoteke);
 
 
-inline int comparePrezimena(const void* a, const void* b) {
-    return strcmp(((Rezervacija*)a)->prezime, ((Rezervacija*)b)->prezime);                      
+inline int usporediRezervacije(const void* a, const void* b) {
+
+    Rezervacija* rezA = *(Rezervacija**)a;
+    Rezervacija* rezB = *(Rezervacija**)b;
+    return strcmp(rezA->prezime, rezB->prezime);
 }
+
 
 #endif
